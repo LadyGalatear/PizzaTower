a3e07d5f34ca3cbbf28005711af48c2be4d737c7
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-01-27 14:14:46 -0800

    Add error messages

--- js/pizza.js
+++ js/pizza.js
@@ -1,1 +1,34 @@
-alert("Hello")
+document.getElementById("pizza-form").onsubmit = () => {
+    clearErrors();
+
+    let isValid = true;
+    // Validate first name
+    let first_name = document.getElementById("first-name").value.trim();
+    if(!first_name) {
+        document.getElementById("err-first-name").style.display = "block";
+        isValid = false;
+    }
+
+    // Validate last name
+    let last_name = document.getElementById("last-name").value.trim();
+    if(!last_name) {
+        document.getElementById("err-last-name").style.display = "block";
+        isValid = false;
+    }
+
+    // Validate email
+    let email = document.getElementById("email-address").value.trim();
+    if(!email) {
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for (let i = 0; i < errors.length; i++) {
+        errors[i].style.display = "none";
+    }
+}
